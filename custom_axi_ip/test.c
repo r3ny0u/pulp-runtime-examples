4e6ef6636dab48b9041d7b1e1e647e513bd95c35
r3ny0u <renyou14@gmail.com>
2024-09-26 21:40:44 +0800

    add delay

--- custom_axi_ip/test.c
+++ custom_axi_ip/test.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <unistd.h>
 
 #define BASE_ADDR 0x1A400000
 
@@ -26,22 +27,27 @@ int main() {
 
     // Write to the registers
     write_reg(reg_en, 0x1);
+    usleep(5);
     write_reg(reg0, 0x1234);
 
     write_reg(reg_en, 0x2);
+    usleep(5);
     write_reg(reg1, 0x5678);
 
     write_reg(reg_en, 0x4);
+    usleep(5);
     write_reg(reg2, 0x9ABC);
 
     // Read from the registers
     uint32_t reg0_val = read_reg(reg0);
     uint32_t reg1_val = read_reg(reg1);
     uint32_t reg2_val = read_reg(reg2);
+    uint32_t reg_en_val = read_reg(reg_en);
 
     printf("Reg0: 0x%X\n", reg0_val);
     printf("Reg1: 0x%X\n", reg1_val);
     printf("Reg2: 0x%X\n", reg2_val);
+    printf("Reg_en: 0x%X\n", reg_en_val);
 
     return 0;
 }
