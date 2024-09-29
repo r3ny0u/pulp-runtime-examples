49edd04dbe72cbd736f393427f6a9948a6c40270
r3ny0u <renyou14@gmail.com>
2024-09-29 23:27:08 +0800

    using int instead

--- custom_axi_ip/test.c
+++ custom_axi_ip/test.c
@@ -4,10 +4,10 @@
 
 #define BASE_ADDR 0x1A400000
 
-#define REG0_OFFSET 0x00
-#define REG1_OFFSET 0x04
-#define REG2_OFFSET 0x08
-#define REG_EN_OFFSET 0x0C
+#define REG0_OFFSET 0x0
+#define REG1_OFFSET 0x4
+#define REG2_OFFSET 0x8
+#define REG_EN_OFFSET 0xC
 
 void set_reg(uint32_t offset, uint32_t *value) {
     // *addr = value;
@@ -29,27 +29,35 @@ uint32_t read_reg(uint32_t offset) {
     return *addr;
 }
 
+void write_reg0 (uint32_t value) {
+    volatile uint32_t *addr = (volatile uint32_t *)(BASE_ADDR + REG0_OFFSET);
+    *addr = value;
+}
+
 int main() {
     // Pointers to the IP registers
     uint32_t reg0[32];
     uint32_t reg1[32];
     uint32_t reg2[32];
-    uint32_t reg_en;
+    uint32_t reg_en = 0;
 
     memset(reg0, 0, sizeof(reg0));
     memset(reg1, 0, sizeof(reg1));
     memset(reg2, 0, sizeof(reg2));
-    memset(reg_en, 0, sizeof(reg_en));
 
     reg0[0] = 3;
-    reg_en = 1;
+    reg_en = 7;
 
     set_reg(REG0_OFFSET, reg0);
-    printf("Reg0_test read: %d\n", read_reg(REG0_OFFSET));
     printf("Reg0_test: %d\n", reg0);
     set_reg(REG1_OFFSET, reg1);
     set_reg(REG2_OFFSET, reg2);
     set_en(reg_en);
+    printf("Reg0_test read: %d\n", read_reg(REG0_OFFSET));
+
+    write_reg0(4294967295);
+    set_en(7);
+    printf("Reg0_test read: %d\n", read_reg(REG0_OFFSET));
 
     return 0;
 }
