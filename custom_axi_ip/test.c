2e81e921a6202e6960541aa46629245e40bdfe65
r3ny0u <renyou14@gmail.com>
2024-10-30 14:29:06 +0800

    trying file

--- custom_axi_ip/test.c
+++ custom_axi_ip/test.c
@@ -1,67 +1,39 @@
 #include <stdio.h>
 #include <stdint.h>
-#include "pulp.h"
-// #include <custom_axi_ip.h>
-// #include <custom_axi_ip_driver.h>
+#include <pulp.h>
 
-// #define BASE_ADDR 0x1A500000
+#define BASE_ADDR 0x1A500000
 
-// #define DIN_OFFSET 0x0
-// #define DOUT_OFFSET 0x4
-// #define ENABLE_OFFSET 0x8
-// #define STATUS_OFFSET 0xc
+void write_data(uint32_t *din) {
+    uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + CUSTOM_AXI_IP_DIN_REG_OFFSET);
+    reg = din;
+    // reg[0] = din[0];
+}
 
-// typedef enum {
-//     IDLE = 0,
-//     BUSY = 1,
-//     DONE = 2,
-//     ERROR = 3
-// } status_e;
+void read_data(uint32_t *dout) {
+    uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + CUSTOM_AXI_IP_DOUT_REG_OFFSET);
+    *dout = *reg;
+}
 
-// uint32_t read_reg(uint32_t offset) {
-//     return *(volatile uint32_t *)(BASE_ADDR + offset);
-// }
+void enable() {
+    uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + CUSTOM_AXI_IP_ENABLE_REG_OFFSET);
+    *reg = 1;
+}
 
-// void write_data(uint32_t data) {
-//     uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + DIN_OFFSET);
-//     *reg = data;
-//     asm volatile ("": : : "memory");
-// }
+void wait() {
+    uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + CUSTOM_AXI_IP_STATUS_REG_OFFSET);
+    uint32_t status;
+    do {
+        status = *reg;
+    } while (status != CUSTOM_AXI_IP_STATUS_STATUS_VALUE_IDLE);
+}
 
-// void enable() {
-//     uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + ENABLE_OFFSET);
-//     *reg = 1;
-//     asm volatile ("": : : "memory");
-// }
-
-// void test_axi_ip() {
-//     uint32_t data = 0x12345678;
-
-//     status_e status;
-//     do {
-//         status = read_reg(STATUS_OFFSET);
-//     } while (status != IDLE);
-
-//     printf("Begin test\n");
-//     write_data(data);
-//     enable();
-
-//     uint32_t output_data = read_reg(DOUT_OFFSET);
-//     uint32_t expected_output_data = data + 1;
-//     uint32_t enable = read_reg(ENABLE_OFFSET);
-//     status_e current_status = read_reg(STATUS_OFFSET);
-
-//     printf("Output data: %x\n", output_data);
-//     printf("Expected output data: %x\n", expected_output_data);
-//     printf("Enable: %x\n", enable);
-//     printf("Status: %x\n", status);
-
-//     if (output_data == expected_output_data) {
-//         printf("Test passed\n");
-//     } else {
-//         printf("Test failed\n");
-//     }
-// }
+void start_test (uint32_t din[1], uint32_t dout[1]) {
+    write_data(din);
+    enable();
+    wait();
+    read_data(dout);
+}
 
 int main() {
     uint32_t din[1];
