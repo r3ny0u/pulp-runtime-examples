fb9210495a7cf858183db754c3a7648ad87cbedb
r3ny0u <renyou14@gmail.com>
2024-10-31 18:32:26 +0800

    change address

--- custom_axi_ip/test.c
+++ custom_axi_ip/test.c
@@ -2,12 +2,11 @@
 #include <stdint.h>
 // #include <pulp.h>
 
-#define BASE_ADDR 0x1A140000
+#define BASE_ADDR 0x1A400000
 #define CUSTOM_AXI_IP_DIN_REG_OFFSET 0x00
 #define CUSTOM_AXI_IP_DOUT_REG_OFFSET 0x04
 #define CUSTOM_AXI_IP_ENABLE_REG_OFFSET 0x08
 #define CUSTOM_AXI_IP_STATUS_REG_OFFSET 0x0C
-#define CUSTOM_AXI_IP_STATUS_STATUS_VALUE_IDLE 0x10
 
 void write_data(uint16_t din) {
     uint16_t volatile *reg = (uint16_t *)(BASE_ADDR + CUSTOM_AXI_IP_DIN_REG_OFFSET);
@@ -25,13 +24,13 @@ void enable() {
     *reg = 1;
 }
 
-void wait() {
-    uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + CUSTOM_AXI_IP_STATUS_REG_OFFSET);
-    uint32_t status;
-    do {
-        status = *reg;
-    } while (status != CUSTOM_AXI_IP_STATUS_STATUS_VALUE_IDLE);
-}
+// void wait() {
+//     uint32_t volatile *reg = (uint32_t *)(BASE_ADDR + CUSTOM_AXI_IP_STATUS_REG_OFFSET);
+//     uint32_t status;
+//     do {
+//         status = *reg;
+//     } while (status != CUSTOM_AXI_IP_STATUS_STATUS_VALUE_IDLE);
+// }
 
 // void start_test (uint32_t din[1], uint32_t dout[1]) {
 void start_test (uint16_t din) {
